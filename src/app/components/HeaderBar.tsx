50b63db8bd8a170f84b750fce1657f7c664ec23d
Edmund Lin <elz.zhenming@gmail.com>
2025-06-07 22:43:30 +0800

    update aesthetics

--- src/app/components/HeaderBar.tsx
+++ src/app/components/HeaderBar.tsx
@@ -7,7 +7,7 @@ export default function HeaderBar({ title }: { title: string }) {
   const router = useRouter();
 
   return (
-    <header className="fixed top-0 left-0 w-full h-16 z-50 flex justify-between items-center px-4 py-3 border-b border-gray-200 bg-[#e31d1a] text-white shadow-md">
+    <header className="fixed top-0 left-0 w-full h-16 z-50 flex justify-between items-center px-4 py-3 bg-[#e31d1a] text-white">
       <h1 className="text-2xl font-semibold">{title}</h1>
       <div className="flex items-center gap-4">
         <button onClick={() => router.push("/search")}>
